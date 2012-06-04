78e566c47d345469dc4bcca1e725d3a675711d4d
Rasmus Svensson <raek@raek.se>
2012-06-04 19:32:58 +0200

    New blank project

---
+++ README.md
@@ -0,0 +1,9 @@
+# ircumflex
+
+This will eventually be a library for writing IRC bots or clients in Clojure.
+
+## License
+
+Copyright © 2012 Rasmus Svensson
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject ircumflex "0.1.0-SNAPSHOT"
+  :description "An IRC bot/client library for Clojure"
+  :url "https://github.com/raek/ircumflex"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
