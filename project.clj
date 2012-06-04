4cfcd5bdc41cd279c76cac466547f904be869797
Rasmus Svensson <raek@raek.se>
2012-06-04 22:22:03 +0200

    Add testing dependency (midje)

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/raek/ircumflex"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
