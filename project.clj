36bc785d07cdf32b78d4edf6fcc8b112c8cbaf34
Rasmus Svensson <raek@raek.se>
2012-06-05 22:58:24 +0200

    Add testing dependency (lazytest)

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :profiles {:dev {:dependencies [[midje "1.4.0"]
+                                  [com.stuartsierra/lazytest "1.2.3"]]
+                   :repositories {"stuart" "http://stuartsierra.com/maven2"}}}
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
   :min-lein-version "2.0.0")
